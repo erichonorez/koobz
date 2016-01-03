f1325da0860c6916b878710020633dfc2d7e3084
Eric Honorez <eric.honorez@gmail.com>
2016-01-03 23:16:04 +0100

    Fixed: jar file launched by koobz-board-start.sh

--- koobz-board/src/main/docker/bin/koobz-board-start.sh
+++ koobz-board/src/main/docker/bin/koobz-board-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-java -jar kanban.jar \
+java -jar koobz-board.jar \
     --spring.datasource.url=$KOOBZ_DB_URL \
     --spring.datasource.username=$KOOBZ_DB_USER \
     --spring.datasource.password=$KOOBZ_DB_PASSWORD
