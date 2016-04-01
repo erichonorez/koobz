047fd2dbff7af14fab6a7da89ed07946ed1380b8
Eric Honorez <eric.honorez@gmail.com>
2016-04-01 21:50:32 +0200

    Uni tests added for the create work item use case

--- koobz-board/src/test/java/org/svomz/apps/koobz/board/applications/BoardApplicationServiceUnitTest.java
+++ koobz-board/src/test/java/org/svomz/apps/koobz/board/applications/BoardApplicationServiceUnitTest.java
@@ -173,6 +173,61 @@ public class BoardApplicationServiceUnitTest {
       assertThat(workItem.getDescription()).isEqualTo(aWorkItemDescription);
     }
 
+    @Test(expected = BoardNotFoundException.class)
+    public void itShouldThrowBoardNotFoundExceptionIfBoardDoesNotExist()
+      throws StageNotInProcessException, BoardNotFoundException {
+      // Given the board with id "35a45cd4-f81f-11e5-9ce9-5e5517507c66" does not exist
+      String boardId = "35a45cd4-f81f-11e5-9ce9-5e5517507c66";
+      String stageId = "35a45cd4-f81f-11e5-9ce9-5e5517507c66";
+
+      BoardRepository boardRepository = mock(BoardRepository.class);
+      when(boardRepository.findOne(boardId)).thenReturn(null);
+
+      BoardApplicationService boardApplicationService = new BoardApplicationService(boardRepository);
+
+      // When user adds a Work Item with title "Drink coffee" to the stage with id "ac329010-f837-11e5-9ce9-5e5517507c66"
+      String aWorkItemTitle = "Drink coffee";
+      String aWorkItemDescription = "At Starbuck";
+
+      WorkItem workItem = boardApplicationService.createWorkItem(
+        boardId,
+        stageId,
+        aWorkItemTitle,
+        aWorkItemDescription
+      );
+
+      // Then I get a BoardNotFoundException
+    }
+
+    @Test(expected = StageNotInProcessException.class)
+    public void itShouldThrowStageNotInProcessExceptionIfStageDoesNotExist()
+      throws StageNotInProcessException, BoardNotFoundException {
+      // Given a board with id "35a45cd4-f81f-11e5-9ce9-5e5517507c66" and title "a board"
+      String boardId = "35a45cd4-f81f-11e5-9ce9-5e5517507c66";
+      String aBoardName = "a board";
+      // And not having a Stage with id "ac329010-f837-11e5-9ce9-5e5517507c66"
+      String stageId = "35a45cd4-f81f-11e5-9ce9-5e5517507c66";
+
+      Board board = new Board(boardId, aBoardName);
+
+      BoardRepository boardRepository = mock(BoardRepository.class);
+      when(boardRepository.findOne(boardId)).thenReturn(board);
+
+      // When user adds a Work Item with title "Drink coffee" to the stage with id "ac329010-f837-11e5-9ce9-5e5517507c66"
+      String aWorkItemTitle = "Drink coffee";
+      String aWorkItemDescription = "At Starbuck";
+
+      BoardApplicationService boardApplicationService = new BoardApplicationService(boardRepository);
+      WorkItem workItem = boardApplicationService.createWorkItem(
+        boardId,
+        stageId,
+        aWorkItemTitle,
+        aWorkItemDescription
+      );
+
+      // Then I get a StageNotInProcessException
+    }
+
   }
 
 }
